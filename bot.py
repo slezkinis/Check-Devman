f938076c6056755e95347ad0cda6ebaf5daca9de
slezkinis <slezkin.is@mail.ru>
2023-05-09 14:18:16 +0300

    Add if __name__ == '__main__'

--- bot.py
+++ bot.py
@@ -4,33 +4,38 @@ from pprint import pprint
 import logging
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
 
-url = 'https://dvmn.org/api/long_polling/'
-headers = {'Authorization': f'Token {os.environ["DVMN_TOKEN"]}'}
-timestamp = ''
-TOKEN = os.environ['TG_TOKEN']
-CHAT_ID = os.environ['TG_CHAT_ID']
-bot = telegram.Bot(token=TOKEN)
-while True:
-    try:
-        if timestamp:
-            params = {'timestamp': timestamp}
-            response = requests.get(url, headers=headers, params=params)
-            timestamp = ''
-        else:
-            response = requests.get(url, headers=headers)
-        if response.json()['status'] == 'timeout':
-            timestamp = response.json()['timestamp_to_request']
-        else:
-            if response.json()['new_attempts'][0]['is_negative']:
-                result = 'К сожалению, в работе нашлись ошибки:('
+def main():
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': f'Token {os.environ["DVMN_TOKEN"]}'}
+    timestamp = ''
+    TOKEN = os.environ['TG_TOKEN']
+    CHAT_ID = os.environ['TG_CHAT_ID']
+    bot = telegram.Bot(token=TOKEN)
+    while True:
+        try:
+            if timestamp:
+                params = {'timestamp': timestamp}
+                response = requests.get(url, headers=headers, params=params)
+                timestamp = ''
             else:
-                result = 'Преподавателю всё понравилось!'
-            text = f'У вас проверили работу "{response.json()["new_attempts"][0]["lesson_title"]}"!\n{result}\nСсылка: {response.json()["new_attempts"][0]["lesson_url"]}'
-            bot.send_message(text=text, chat_id=CHAT_ID)            
-    except requests.exceptions.ReadTimeout:
-        logging.warning('Превышено время ожидания! Делаю повторный запрос')
-    except requests.exceptions.ConnectionError:
-        logging.error('Нет подключения к сети!')
+                response = requests.get(url, headers=headers)
+            if response.json()['status'] == 'timeout':
+                timestamp = response.json()['timestamp_to_request']
+            else:
+                if response.json()['new_attempts'][0]['is_negative']:
+                    result = 'К сожалению, в работе нашлись ошибки:('
+                else:
+                    result = 'Преподавателю всё понравилось!'
+                text = f'У вас проверили работу "{response.json()["new_attempts"][0]["lesson_title"]}"!\n{result}\nСсылка: {response.json()["new_attempts"][0]["lesson_url"]}'
+                bot.send_message(text=text, chat_id=CHAT_ID)            
+        except requests.exceptions.ReadTimeout:
+            logging.warning('Превышено время ожидания! Делаю повторный запрос')
+        except requests.exceptions.ConnectionError:
+            logging.error('Нет подключения к сети!')
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    main()
